af859678a817b09433ff644a6e711d4bf218afd1
Guy Blelloch <guyb@cs.cmu.edu>
2018-11-30 18:12:42 -0500

    changes

--- scheduler.h
+++ scheduler.h
@@ -1,8 +1,8 @@
-// from Arora, Blumofe, Plaxton
 #include <omp.h>
 
 using namespace std;
 
+// from Arora, Blumofe, Plaxton
 template <typename Job>
 struct Deque {
   using qidx = int;
@@ -28,16 +28,16 @@ struct Deque {
     std::atomic_thread_fence(std::memory_order_release);
   }
 
-  Deque() : bot(-1) {
+  Deque() : bot(0) {
     age.pair.tag = 0;
-    age.pair.top = -1;
+    age.pair.top = 0;
   }
     
   void push_bottom(Job* node) {
     qidx local_bot = bot; // atomic load
-    local_bot += 1;
     deq[local_bot] = node; // shared store
     fence();
+    local_bot += 1;
     bot = local_bot; // shared store
     fence(); // probably not needed
   }
@@ -51,8 +51,7 @@ struct Deque {
     Job* node = deq[old_age.pair.top]; // atomic load
     new_age.unit = old_age.unit;
     new_age.pair.top = new_age.pair.top + 1;
-    cas(&(age.unit), old_age.unit, new_age.unit); // cas
-    if (old_age.unit == new_age.unit)
+    if (cas(&(age.unit), old_age.unit, new_age.unit))  // cas
       return node;
     return NULL;
   }
@@ -60,25 +59,21 @@ struct Deque {
   Job* pop_bottom() {
     age_t old_age, new_age;
     qidx local_bot = bot; // atomic load
-    cout << "bot is " << local_bot << endl;
-    if (local_bot == -1) 
+    if (local_bot == 0) 
       return NULL;
     local_bot = local_bot - 1;
     bot = local_bot; // shared store
     fence();
     Job* node = deq[local_bot]; // atomic load
     old_age.unit = age.unit; // atomic load
-    cout << "here2 " << (node == NULL) << ", " << local_bot << ", " << old_age.pair.top << endl;
     if (local_bot > old_age.pair.top)
       return node;
-    bot = -1; // shared store
+    bot = 0; // shared store
     fence();
-    new_age.pair.top = -1;
+    new_age.pair.top = 0;
     new_age.pair.tag = old_age.pair.tag + 1;
     if (local_bot == old_age.pair.top) {
-      cout << "check it out " << endl;
       if (cas(&(age.unit), old_age.unit, new_age.unit)) { // cas
-	cout << "ooio" << endl;
 	return node;
       }
     }
@@ -90,60 +85,56 @@ struct Deque {
 
 template <typename Job>
 struct scheduler {
-  int num_deques;
-  Deque<Job>* deques;
-  int finished_flag;
 
 public:
   scheduler() {
-    num_deques = num_workers();
+    num_deques = 2*num_workers();
     deques = new Deque<Job>[num_deques];
     finished_flag = 0;
   }
 
-  void run(Job* job) {
-    //(*job)();
-    cout << job << endl;
+  ~scheduler() {
+    delete[] deques;
+  }
+
+  void run(Job* job, int num_threads = 0) {
     deques[0].push_bottom(job);
-    cout << deques[0].deq[0] << endl;
     auto finished = [&] () {return finished_flag > 0;};
-    omp_set_num_threads(1);
-    _Pragma("omp parallel");
-    {
-      _Pragma("omp single");
-      cout << worker_id() << endl;
-      wait(finished);
-      _Pragma("omp single");
-      cout << "end: " << worker_id() << endl;
-    }
+    if (num_threads > 0 && num_threads < 2 * num_workers())
+      omp_set_num_threads(num_threads);
+    #pragma omp parallel
+    wait(finished);
   }
     
   void spawn(Job* job) {
     int id = worker_id();
-    deques[id].push_bottom[job];
+    deques[id].push_bottom(job);
   }
 
   template <typename F>
   void wait(F finished) {
     while (1) {
-      cout << "try job: " << endl;
       Job* job = get_job(finished);
       if (!job) return;
-      cout << "got job: " << job <<  endl;
       (*job)();
-      cout << "end job" << endl;
     }
+    //while (!finished()) {
+    //  std::yied
   }
 
   void finish() {finished_flag = 1;}
 
-private:
-  
   Job* try_pop() {
     int id = worker_id();
-    cout << "worker id: " << id <<  endl;
     return deques[id].pop_bottom();
   }
+
+
+private:
+
+  int num_deques;
+  Deque<Job>* deques;
+  int finished_flag;
 
   Job* try_steal() {
     int target = rand() % num_deques;
@@ -154,9 +145,7 @@ private:
   Job* get_job(F finished) {
     if (finished()) return NULL;
     Job* job = try_pop();
-    cout << "try pop: " << (job == NULL) <<  endl;
     if (job) return job;
-    cout << "here: " << (job == NULL) <<  endl;
     while (1) {
       if (finished()) return NULL;
       job = try_steal();
@@ -180,18 +169,25 @@ struct fork_join_scheduler {
   }
 
   template <typename J>
-  void run(J thunk) {
-    Job job = [&] () {cout << "foofoo" << endl; thunk(); sched->finish();};
-    sched->run(&job);
+  void run(J thunk, int num_threads=0) {
+    Job job = [&] () {thunk(); sched->finish();};
+    sched->run(&job,num_threads);
   }
     
   template <typename L, typename R>
   void pardo(L left, R right) {
-    int cnt = 0;
-    Job right_job = [&] () {right(); cnt = 1;};
+    bool right_done = false;
+    bool stolen = false;
+    Job right_job = [&] () {
+      stolen = true; right(); right_done = true;};
     sched->spawn(&right_job);
     left();
-    auto finished = [&] () {return cnt == 1;};
+    if (!stolen) {
+      Job* job = sched->try_pop();
+      if (job != &right_job) sched->spawn(job);
+      else { right(); return;}
+    }
+    auto finished = [&] () {return right_done;};
     sched->wait(finished);
   }
   
