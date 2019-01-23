b58b996ceade44671beca9bc3377ebdbda7272a4
Guy Blelloch <guyb@cs.cmu.edu>
2019-01-23 18:18:33 -0500

    fixed some warnings

--- parallel.h
+++ parallel.h
@@ -45,7 +45,7 @@ static void par_do(Lf left, Rf right, bool conservative=false);
 
 static int num_workers() {return __cilkrts_get_nworkers();}
 static int worker_id() {return __cilkrts_get_worker_number();}
-static void set_num_workers(int n) {
+inline void set_num_workers(int n) {
   __cilkrts_end_cilk();
   std::stringstream ss; ss << n;
   if (0 != __cilkrts_set_param("nworkers", ss.str().c_str())) {
@@ -55,7 +55,7 @@ static void set_num_workers(int n) {
 }
 
 template <typename F>
-static void parallel_for(long start, long end, F f,
+inline void parallel_for(long start, long end, F f,
 			 long granularity,
 			 bool conservative) {
   if (granularity == 0)
@@ -72,7 +72,7 @@ static void parallel_for(long start, long end, F f,
 }
 
 template <typename Lf, typename Rf>
-static void par_do(Lf left, Rf right, bool conservative) {
+inline void par_do(Lf left, Rf right, bool conservative) {
     cilk_spawn right();
     left();
     cilk_sync;
@@ -83,12 +83,12 @@ static void par_do(Lf left, Rf right, bool conservative) {
 #include <omp.h>
 #define PAR_GRANULARITY 200000
 
-static int num_workers() { return omp_get_max_threads(); }
-static int worker_id() { return omp_get_thread_num(); }
-static void set_num_workers(int n) { omp_set_num_threads(n); }
+inline int num_workers() { return omp_get_max_threads(); }
+inline int worker_id() { return omp_get_thread_num(); }
+inline void set_num_workers(int n) { omp_set_num_threads(n); }
 
 template <class F>
-static void parallel_for(long start, long end, F f,
+inline void parallel_for(long start, long end, F f,
 			 long granularity,
 			 bool conservative) {
   _Pragma("omp parallel for")
@@ -96,7 +96,7 @@ static void parallel_for(long start, long end, F f,
 }
 
 template <typename Lf, typename Rf>
-static void par_do(Lf left, Rf right, bool conservative) {
+inline void par_do(Lf left, Rf right, bool conservative) {
 #pragma omp task
     left();
 #pragma omp task
@@ -105,7 +105,7 @@ static void par_do(Lf left, Rf right, bool conservative) {
 }
 
 template <typename Job>
-static void parallel_run(Job job, int num_threads=0) {
+inline void parallel_run(Job job, int num_threads=0) {
   job();
 }
 
@@ -122,45 +122,45 @@ const int atexit_result = std::atexit(destroy_fj);
 
 #define PAR_GRANULARITY 512
 
-static int num_workers() {
+inline int num_workers() {
   return fj.num_workers();
 }
 
-static int worker_id() {
+inline int worker_id() {
   return fj.worker_id();
 }
 
-static void set_num_workers(int n) {
+inline void set_num_workers(int n) {
   fj.set_num_workers(n);
 }
 
 template <class F>
-static void parallel_for(long start, long end, F f,
+inline void parallel_for(long start, long end, F f,
 			 long granularity,
 			 bool conservative) {
   fj.parfor(start, end, f, granularity, conservative);
 }
 
 template <typename Lf, typename Rf>
-static void par_do(Lf left, Rf right, bool conservative) {
+inline void par_do(Lf left, Rf right, bool conservative) {
   return fj.pardo(left, right, conservative);
 }
 
 template <typename Job>
-static void parallel_run(Job job, int num_threads=0) {
+inline void parallel_run(Job job, int num_threads=0) {
   job();
 }
 
 // c++
 #else
 
-static int num_workers() { return 1;}
-static int worker_id() { return 0;}
-static void set_num_workers(int n) { ; }
+inline int num_workers() { return 1;}
+inline int worker_id() { return 0;}
+inline void set_num_workers(int n) { ; }
 #define PAR_GRANULARITY 1000
 
 template <class F>
-static void parallel_for(long start, long end, F f,
+inline void parallel_for(long start, long end, F f,
 			 long granularity,
 			 bool conservative) {
   for (long i=start; i<end; i++) {
@@ -169,12 +169,12 @@ static void parallel_for(long start, long end, F f,
 }
 
 template <typename Lf, typename Rf>
-static void par_do(Lf left, Rf right, bool conservative) {
+inline void par_do(Lf left, Rf right, bool conservative) {
   left(); right();
 }
 
 template <typename Job>
-static void parallel_run(Job job, int num_threads=0) {
+inline void parallel_run(Job job, int num_threads=0) {
   job();
 }
 
