4167368a55120470244c85bfec8b90e931ac0043
Guy Blelloch <guyb@cs.cmu.edu>
2019-04-27 11:05:06 -0400

    changes

--- examples/mcss.cpp
+++ examples/mcss.cpp
@@ -1,6 +1,7 @@
 #include "sequence.h"
 #include "random.h"
 #include "get_time.h"
+#include "parse_command_line.h"
 
 using namespace std;
 using namespace pbbs;
@@ -9,8 +10,8 @@ using namespace pbbs;
 
 // 10x improvement by using delayed sequence
 template <class Seq>
-typename Seq::T mcss(Seq const &A) {
-  using T = typename Seq::T;
+typename Seq::value_type mcss(Seq const &A) {
+  using T = typename Seq::value_type;
   using TT = tuple<T,T,T,T>;
   auto f = [&] (TT a, TT b) {
     T aa, pa, sa, ta, ab, pb, sb, tb;
@@ -27,13 +28,18 @@ typename Seq::T mcss(Seq const &A) {
   return get<0>(r);
 }
 
-int main() {
-  using T = double;
+int main (int argc, char *argv[]) {
+  commandLine P(argc, argv, "[-r <rounds>] [-n <size>]");
+  int rounds = P.getOptionIntValue("-r", 3);
   size_t n = 100000000;
+  n = P.getOptionLongValue("-n", 1);
+  timer t("MCSS");
+  using T = double;
+
   pbbs::random r(0);
   sequence<T> A(n, [&] (size_t i) {return (T) (r[i]%n - n/2);});
   T result;
-  for (int i=0; i < 5; i++) {
+  for (int i=0; i < rounds; i++) {
     result = mcss(A);
     t.next("Total");
   }
